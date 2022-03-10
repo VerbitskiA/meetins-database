cfdda881f9eb4bfc4498c34dde19aa4e545e6336
VerbitskiA <eternity.2019@mail.ru>
2022-03-10 18:58:44 +0300

    Изменил структуру табли EventsToUsers.

--- Events/pgsql.meetins_test_db/Tables/[Events].[EventsToUsers]/[Events].[EventsToUsers]_CreateTable.sql
+++ Events/pgsql.meetins_test_db/Tables/[Events].[EventsToUsers]/[Events].[EventsToUsers]_CreateTable.sql
@@ -2,5 +2,6 @@ create table if not exists "Events"."EventsToUsers" (
 	"EventToUserId" uuid primary key,
 	"EventId" uuid references "Events"."Events"("EventId"),
 	"UserId"  uuid references "User"."Users"("UserId"),
-	"SubscriptionDate" timestamp not null default now()
+	"SubscriptionDate" timestamp not null default now(),
+	"IsUserSubscribed" boolean not null default false
 );
