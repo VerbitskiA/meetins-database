dbefceb182280c3ae7310dbd0976411ba0c81e56
Yana Latysh <123@123.by>
2022-04-21 23:22:56 +0300

    Изменена колонка для таблицы обращений.

--- dbo/pgsql.meetins_test_db/Tables/[dbo].[Reports]/[dbo].[Reports]_CreateTable.sql
+++ dbo/pgsql.meetins_test_db/Tables/[dbo].[Reports]/[dbo].[Reports]_CreateTable.sql
@@ -1,7 +1,7 @@
 create table if not exists dbo."Reports" (
 	"ReportId" uuid primary key,
 	"UserId" uuid REFERENCES "User"."Users"("UserId") ON DELETE CASCADE,
-	"Status" varchar(50) null,
+	"IsOpened" bool,
 	"Topic" varchar not null default '',
 	"Text" varchar(500) not null default '',
 	"Date" timestamp not null default now()	
